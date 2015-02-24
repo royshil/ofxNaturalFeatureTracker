730759f4175729d2854d1922703d2dd860c0fd13
Roy Shilkrot <roy.shil@gmail.com>
2015-02-23 23:23:04 -0500

    fixed license

--- src/ImageTrackerLib.mm
+++ src/ImageTrackerLib.mm
@@ -6,7 +6,7 @@
  *
  *  The MIT License (MIT)
  *
- *  Copyright (c) <year> <copyright holders>
+ *  Copyright (c) 2015 Roy Shilkrot and Valentin Heun
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
